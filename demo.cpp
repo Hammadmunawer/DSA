35c7a3ce6a8ec4d5b71bc8e8e8119765deac0e00
Hammad Ali <hammadmunawer2004@gmail.com>
2026-01-28 03:57:36 +0500

    initial commit

---
+++ demo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout <<"hammad";
+cout<<"talha ";
+int x;
+cin>>x;
+cout<<x;
+cout<<"hammad";
+int z;
+cin>>z;
+cout<<z;
+int y;
+cin>>y;
+cout<<y;
+    return 0;
+}
